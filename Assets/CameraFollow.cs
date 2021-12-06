ca241d83d6dcbe0ccd81f6a7a83828e1c842f9e2
fildakrapa <90609100+fildakrapa@users.noreply.github.com>
2021-12-06 12:02:40 +0100

    Add files via upload

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+  public Transform target;
+  public Vector3 offset;
+  [Range(1,10)]
+  public float smoothFactor;
+
+
+  private void FixedUpdate()
+  {
+      
+      Follow();
+  }
+  public void Follow()
+  {
+      Vector3 targetPosition = target.position + offset;
+      Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition,smoothFactor*Time.fixedDeltaTime);
+      transform.position = targetPosition;
+  }
+}
